7db7a07cfa74a35ff86a3042eb40b59ac49ca058
Takumi Nishida <tnishida1@mail.csuchico.edu>
2018-09-09 16:38:16 -0700

    Initial Commit

---
+++ Faver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Faver
+//
+//  Created by Takumi Nishida on 9/9/18.
+//  Copyright © 2018 Takumi Nishida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
