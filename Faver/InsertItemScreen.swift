4a4065a252bb8a8f3e888ec0adb0a716af6effe1
Takumi Nishida <tnishida1@mail.csuchico.edu>
2018-09-13 17:54:46 -0700

    init

---
+++ Faver/InsertItemScreen.swift
@@ -0,0 +1,38 @@
+//
+//  InsertItemScreen.swift
+//  Faver
+//
+//  Created by Takumi Nishida on 9/9/18.
+//  Copyright © 2018 Takumi Nishida. All rights reserved.
+//
+
+import UIKit
+
+class InsertItemScreen: UIViewController {
+
+    @IBAction func DoneButton(_ sender: Any) {
+    }
+    @IBOutlet weak var TextBox: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
